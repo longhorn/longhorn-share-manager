2ab80c2344faa11b85c90475888b1f69c098aa99
Derek Su <derek.su@suse.com>
2023-11-15 12:39:32 +0800

    proto: introduce Mount method
    Longhorn 7106
    
    Signed-off-by: Derek Su <derek.su@suse.com>
    (cherry picked from commit 2176cd684b0fc10043111a550608517f1b2ac3e7)

--- integration/rpc/share_manager/smrpc_pb2_grpc.py
+++ integration/rpc/share_manager/smrpc_pb2_grpc.py
@@ -25,6 +25,11 @@ class ShareManagerServiceStub(object):
         request_serializer=google_dot_protobuf_dot_empty__pb2.Empty.SerializeToString,
         response_deserializer=google_dot_protobuf_dot_empty__pb2.Empty.FromString,
         )
+    self.Mount = channel.unary_unary(
+        '/ShareManagerService/Mount',
+        request_serializer=google_dot_protobuf_dot_empty__pb2.Empty.SerializeToString,
+        response_deserializer=google_dot_protobuf_dot_empty__pb2.Empty.FromString,
+        )
 
 
 class ShareManagerServiceServicer(object):
@@ -45,6 +50,13 @@ class ShareManagerServiceServicer(object):
     context.set_details('Method not implemented!')
     raise NotImplementedError('Method not implemented!')
 
+  def Mount(self, request, context):
+    # missing associated documentation comment in .proto file
+    pass
+    context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+    context.set_details('Method not implemented!')
+    raise NotImplementedError('Method not implemented!')
+
 
 def add_ShareManagerServiceServicer_to_server(servicer, server):
   rpc_method_handlers = {
@@ -55,6 +67,11 @@ def add_ShareManagerServiceServicer_to_server(servicer, server):
       ),
       'Unmount': grpc.unary_unary_rpc_method_handler(
           servicer.Unmount,
+          request_deserializer=google_dot_protobuf_dot_empty__pb2.Empty.FromString,
+          response_serializer=google_dot_protobuf_dot_empty__pb2.Empty.SerializeToString,
+      ),
+      'Mount': grpc.unary_unary_rpc_method_handler(
+          servicer.Mount,
           request_deserializer=google_dot_protobuf_dot_empty__pb2.Empty.FromString,
           response_serializer=google_dot_protobuf_dot_empty__pb2.Empty.SerializeToString,
       ),
